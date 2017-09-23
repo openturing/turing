54a2c41e69c08b25ac6b4e913a15d131765d8aaf
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2017-09-23 01:21:08 -0300

    - Gradle Wrapper

--- src/main/resources/public/js/build/extract/home/TurHomeCtrl.js
+++ src/main/resources/public/js/build/extract/home/TurHomeCtrl.js
@@ -1,11 +1,6 @@
-turingApp.controller('TurHomeCtrl', [
-	"$scope",
-	"$http",
-	"$window",
-	"$state",
-	"$rootScope",
-	"$translate",
-	function ($scope, $http, $window, $state, $rootScope, $translate) {
-		$scope.accesses = null;
-		$rootScope.$state = $state;		
-	}]);
+turingApp.controller('TurHomeCtrl', [ "$scope", "$http", "$window", "$state",
+		"$rootScope", "$translate",
+		function($scope, $http, $window, $state, $rootScope, $translate) {
+			$scope.accesses = null;
+			$rootScope.$state = $state;
+		} ]);
