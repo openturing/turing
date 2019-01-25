b02daf11830dcabcb98ac918f039085436c676a9
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2019-01-24 22:42:28 -0200

    - Removed SubModule

--- turing-nlp/src/main/java/com/viglet/turing/persistence/repository/nlp/TurNLPInstanceRepositoryCustom.java
+++
@@ -1,8 +0,0 @@
-package com.viglet.turing.persistence.repository.nlp;
-
-import com.viglet.turing.persistence.model.nlp.TurNLPInstance;
-
-public interface TurNLPInstanceRepositoryCustom {
-
-	TurNLPInstance saveAndAssocEntity(TurNLPInstance turNLPInstance);
-}
