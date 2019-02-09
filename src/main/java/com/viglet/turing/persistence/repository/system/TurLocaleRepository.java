391869d4c78fe64940c484d8827ba17bd105ffc1
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2019-02-09 21:15:50 -0200

    - CacheEvict fixed

--- src/main/java/com/viglet/turing/persistence/repository/system/TurLocaleRepository.java
+++ src/main/java/com/viglet/turing/persistence/repository/system/TurLocaleRepository.java
@@ -5,6 +5,7 @@ import com.viglet.turing.persistence.model.system.TurLocale;
 import java.util.List;
 
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
