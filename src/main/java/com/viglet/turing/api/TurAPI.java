fac5739d83e477cebb596be8724d72a44ea14ebf
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2019-03-08 19:10:24 -0300

    - OpenNLP using List instead of JSONArray

--- src/main/java/com/viglet/turing/api/TurAPI.java
+++ src/main/java/com/viglet/turing/api/TurAPI.java
@@ -2,7 +2,6 @@ package com.viglet.turing.api;
 
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
