f654281566f546c2e3bc936f4010fe56230fda90
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2019-03-12 19:11:19 -0300

    - GNU License Header

--- src/main/java/com/viglet/turing/spring/TurStaticResourceConfiguration.java
+++ src/main/java/com/viglet/turing/spring/TurStaticResourceConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2018 Alexandre Oliveira <alexandre.oliveira@viglet.com> 
+ * Copyright (C) 2016-2019 the original author or authors. 
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
