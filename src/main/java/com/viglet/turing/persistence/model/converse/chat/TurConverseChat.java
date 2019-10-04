65b0ac6864276fdd7c1cb87ae2e4ecac0d9d1f40
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2019-10-04 13:07:32 -0300

    - Converse: Chat and ChatResponse Models

---
+++ src/main/java/com/viglet/turing/persistence/model/converse/chat/TurConverseChat.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2016-2019 the original author or authors. 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.viglet.turing.persistence.model.converse.chat;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+/**
+ * The persistent class for the turConverseChat database table.
+ * 
+ */
+@Entity
+@Table(name = "turConverseChat")
+@NamedQuery(name = "TurConverseChat.findAll", query = "SELECT cc FROM TurConverseChat cc")
+public class TurConverseChat  implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GenericGenerator(name = "UUID", strategy = "com.viglet.turing.jpa.TurUUIDGenerator")
+	@GeneratedValue(generator = "UUID")
+	@Column(name = "id", updatable = false, nullable = false)
+	private String id;
+		
+	private String session;
+	
+	private String agentId;
+	
+	@OneToMany(mappedBy = "chat", orphanRemoval = true, fetch = FetchType.LAZY)
+	@Cascade({ CascadeType.ALL })
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	private List<TurConverseChatResponse> responses = new ArrayList<>();
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getSession() {
+		return session;
+	}
+
+	public void setSession(String session) {
+		this.session = session;
+	}
+
+	public String getAgentId() {
+		return agentId;
+	}
+
+	public void setAgentId(String agentId) {
+		this.agentId = agentId;
+	}
+
+	public List<TurConverseChatResponse> getResponses() {
+		return this.responses;
+	}
+
+	public void setResponses(List<TurConverseChatResponse> responses) {
+		this.responses.clear();
+		if (responses != null) {
+			this.responses.addAll(responses);
+		}
+	}
+	
+}
---
+++ src/main/java/com/viglet/turing/persistence/model/converse/chat/TurConverseChatResponse.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright (C) 2016-2019 the original author or authors. 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.viglet.turing.persistence.model.converse.chat;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import com.viglet.turing.persistence.model.converse.intent.TurConverseIntent;
+
+/**
+ * The persistent class for the turConverseChatResponse database table.
+ * 
+ */
+@Entity
+@Table(name = "turConverseChatResponse")
+@NamedQuery(name = "TurConverseChatResponse.findAll", query = "SELECT ccr FROM TurConverseChatResponse ccr")
+public class TurConverseChatResponse implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GenericGenerator(name = "UUID", strategy = "com.viglet.turing.jpa.TurUUIDGenerator")
+	@GeneratedValue(generator = "UUID")
+	@Column(name = "id", updatable = false, nullable = false)
+	private String id;
+
+	private int position;
+
+	private String text;
+
+	private boolean isUser;
+
+	private String intentId;
+
+	private String actionName;
+
+	private String parameterName;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "chat_id")
+	private TurConverseIntent chat;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public void setPosition(int position) {
+		this.position = position;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public boolean isUser() {
+		return isUser;
+	}
+
+	public void setUser(boolean isUser) {
+		this.isUser = isUser;
+	}
+
+	public String getIntentId() {
+		return intentId;
+	}
+
+	public void setIntentId(String intentId) {
+		this.intentId = intentId;
+	}
+
+	public String getActionName() {
+		return actionName;
+	}
+
+	public void setActionName(String actionName) {
+		this.actionName = actionName;
+	}
+
+	public String getParameterName() {
+		return parameterName;
+	}
+
+	public void setParameterName(String parameterName) {
+		this.parameterName = parameterName;
+	}
+
+	public TurConverseIntent getChat() {
+		return chat;
+	}
+
+	public void setChat(TurConverseIntent chat) {
+		this.chat = chat;
+	}
+
+}
