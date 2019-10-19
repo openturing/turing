7187e5de3793b24f19a4e7bf4ee8c4968eff70d9
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2019-10-19 10:45:46 -0300

    - Converse: Import into Turing

--- src/main/java/com/viglet/turing/converse/exchange/TurConverseImportExchange.java
+++ src/main/java/com/viglet/turing/converse/exchange/TurConverseImportExchange.java
@@ -40,7 +40,25 @@ import com.viglet.turing.converse.exchange.agent.TurConverseAgentExchange;
 import com.viglet.turing.converse.exchange.entity.TurConverseEntityEntriesExchange;
 import com.viglet.turing.converse.exchange.entity.TurConverseEntityExchange;
 import com.viglet.turing.converse.exchange.intent.TurConverseIntentExchange;
+import com.viglet.turing.converse.exchange.intent.TurConverseIntentMessageExchange;
+import com.viglet.turing.converse.exchange.intent.TurConverseIntentPhraseDataExchange;
+import com.viglet.turing.converse.exchange.intent.TurConverseIntentPhraseExchange;
 import com.viglet.turing.converse.exchange.intent.TurConverseIntentPhrasesExchange;
+import com.viglet.turing.converse.exchange.intent.TurConverseIntentResponseExchange;
+import com.viglet.turing.persistence.model.converse.TurConverseAgent;
+import com.viglet.turing.persistence.model.converse.intent.TurConverseIntent;
+import com.viglet.turing.persistence.model.converse.intent.TurConversePhrase;
+import com.viglet.turing.persistence.model.converse.intent.TurConverseResponse;
+import com.viglet.turing.persistence.repository.converse.TurConverseAgentRepository;
+import com.viglet.turing.persistence.repository.converse.entity.TurConverseEntityRepository;
+import com.viglet.turing.persistence.repository.converse.entity.TurConverseEntityTermRepository;
+import com.viglet.turing.persistence.repository.converse.intent.TurConverseContextRepository;
+import com.viglet.turing.persistence.repository.converse.intent.TurConverseIntentRepository;
+import com.viglet.turing.persistence.repository.converse.intent.TurConverseParameterRepository;
+import com.viglet.turing.persistence.repository.converse.intent.TurConversePhraseRepository;
+import com.viglet.turing.persistence.repository.converse.intent.TurConversePromptRepository;
+import com.viglet.turing.persistence.repository.converse.intent.TurConverseResponseRepository;
+import com.viglet.turing.persistence.repository.se.TurSEInstanceRepository;
 import com.viglet.turing.util.TurUtils;
 
 @Component
@@ -49,6 +67,26 @@ public class TurConverseImportExchange {
 
 	@Autowired
 	private TurUtils turUtils;
+	@Autowired
+	private TurConverseAgentRepository turConverseAgentRepository;
+	@Autowired
+	private TurConverseIntentRepository turConverseIntentRepository;
+	@Autowired
+	private TurConverseContextRepository turConverseContextRepository;
+	@Autowired
+	private TurConversePhraseRepository turConversePhraseRepository;
+	@Autowired
+	private TurConverseResponseRepository turConverseResponseRepository;
+	@Autowired
+	private TurConverseParameterRepository turConverseParameterRepository;
+	@Autowired
+	private TurConversePromptRepository turConversePromptRepository;
+	@Autowired
+	private TurSEInstanceRepository turSEInstanceRepository;
+	@Autowired
+	private TurConverseEntityRepository turConverseEntityRepository;
+	@Autowired
+	private TurConverseEntityTermRepository turConverseEntityTermRepository;
 
 	private Map<String, Object> shObjects = new HashMap<String, Object>();
 	private Map<String, List<String>> shChildObjects = new HashMap<String, List<String>>();
@@ -59,8 +97,8 @@ public class TurConverseImportExchange {
 		File parentExtractFolder = null;
 
 		if (extractFolder != null) {
-			// Check if export.json exists, if it is not exist try access a sub directory
-			if (!(new File(extractFolder, "export.json").exists()) && (extractFolder.listFiles().length == 1)) {
+			// Check if agent.json exists, if it is not exist try access a sub directory
+			if (!(new File(extractFolder, "agent.json").exists()) && (extractFolder.listFiles().length == 1)) {
 				for (File fileOrDirectory : extractFolder.listFiles()) {
 					if (fileOrDirectory.isDirectory() && new File(fileOrDirectory, "agent.json").exists()) {
 						parentExtractFolder = extractFolder;
@@ -75,6 +113,19 @@ public class TurConverseImportExchange {
 					new FileInputStream(extractFolder.getAbsolutePath().concat(File.separator + "agent.json")),
 					TurConverseAgentExchange.class);
 			System.out.println(turConverseAgentExchange.getDescription());
+
+			TurConverseAgent turConverseAgent = new TurConverseAgent();
+			turConverseAgent.setName(multipartFile.getOriginalFilename().replace(".zip", ""));
+			turConverseAgent.setDescription(turConverseAgentExchange.getDescription());
+			turConverseAgent.setCore("converse");
+			turConverseAgent.setTurSEInstance(turSEInstanceRepository.findAll().get(0));
+			if (turConverseAgentExchange.getLanguage().equals("pt-br")) {
+				turConverseAgent.setLanguage("pt_BR");
+			} else {
+				turConverseAgent.setLanguage(turConverseAgentExchange.getLanguage());
+			}
+
+			turConverseAgentRepository.save(turConverseAgent);
 
 			TurConverseEntityExchange turConverseEntityExchange = mapper.readValue(
 					new FileInputStream(extractFolder.getAbsolutePath()
@@ -92,7 +143,7 @@ public class TurConverseImportExchange {
 
 			final File folder = new File(extractFolder.getAbsolutePath().concat(File.separator + "intents"));
 
-			listFilesForFolder(folder);
+			listFilesForFolder(folder, turConverseAgent);
 
 			try {
 				FileUtils.deleteDirectory(extractFolder);
@@ -108,7 +159,7 @@ public class TurConverseImportExchange {
 		}
 	}
 
-	public void listFilesForFolder(final File folder) {
+	public void listFilesForFolder(final File folder, TurConverseAgent turConverseAgent) {
 
 		final ObjectMapper mapper = new ObjectMapper().enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
 
@@ -120,11 +171,48 @@ public class TurConverseImportExchange {
 					TurConverseIntentPhrasesExchange turConverseIntentPhrasesExchange = mapper
 							.readValue(new FileInputStream(fileEntry), TurConverseIntentPhrasesExchange.class);
 					System.out.println(turConverseIntentPhrasesExchange.get(0).getId());
-				}
-				else {
+				} else {
+
 					TurConverseIntentExchange turConverseIntentExchange = mapper
 							.readValue(new FileInputStream(fileEntry), TurConverseIntentExchange.class);
 					System.out.println(turConverseIntentExchange.getName());
+
+					// Intent
+					TurConverseIntent turConverseIntent = new TurConverseIntent();
+					turConverseIntent.setName(turConverseIntentExchange.getName());
+					turConverseIntent.setAgent(turConverseAgent);
+//					turConverseIntent.setActionName();
+					turConverseIntentRepository.save(turConverseIntent);
+
+					// Responses
+					for (TurConverseIntentResponseExchange response : turConverseIntentExchange.getResponses()) {
+						for (TurConverseIntentMessageExchange message : response.getMessages()) {
+							if (message.getSpeech().size() > 0) {
+								TurConverseResponse turConverseResponse = new TurConverseResponse(
+										message.getSpeech().get(0));
+								turConverseResponse.setIntent(turConverseIntent);
+								turConverseResponseRepository.save(turConverseResponse);
+							}
+
+						}
+					}
+					File phrasesFile = new File(fileEntry.getAbsolutePath().replace(".json", "_usersays_pt-br.json"));
+					if (phrasesFile.exists()) {
+						TurConverseIntentPhrasesExchange turConverseIntentPhrasesExchange = mapper
+								.readValue(new FileInputStream(phrasesFile), TurConverseIntentPhrasesExchange.class);
+
+						// Phrases
+						for (TurConverseIntentPhraseExchange phrase : turConverseIntentPhrasesExchange) {
+							for (TurConverseIntentPhraseDataExchange data : phrase.getData()) {
+								TurConversePhrase turConversePhrase = new TurConversePhrase(data.getText());
+								turConversePhrase.setIntent(turConverseIntent);
+								turConversePhraseRepository.save(turConversePhrase);
+							}
+						}
+
+					} else {
+						System.out.println("Usersays not exists: " + phrasesFile.getAbsolutePath());
+					}
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
