9da203f8a9c2a57163dcba5e5488c4435d1ac241
Alexandre Oliveira <alexandre.oliveira@viglet.com>
2020-11-20 12:02:52 -0300

    Spring Boot 2.4.0 and Gradle 6.7.1

--- src/main/resources/public/js/sn/build/aggregate/turSNFeature.js
+++ src/main/resources/public/js/sn/build/aggregate/turSNFeature.js
@@ -1,3 +1,42 @@
+turingSNApp.factory('turSNSearch', [
+		'$http',
+		'turAPIServerService',
+		function($http, turAPIServerService) {
+
+			return {
+				search : function(turSiteName, query, page, _setlocale, sort, fq, tr) {
+					var data = {
+						'q' : query,
+						'p' : page,
+						'_setlocale' : _setlocale,
+						'sort' : sort,
+						'fq[]' : fq,
+						'tr[]' : tr
+					};
+					var config = {
+						params : data,
+						headers : {
+							'Accept' : 'application/json'
+						}
+					};
+
+					return $http.get(turAPIServerService.get().concat(
+							'/sn/' + turSiteName + '/search'), config);
+				},
+				searchURL : function(url) {
+					urlFormatted = url.replace("/api/",
+							"/");
+					var config = {
+						headers : {
+							'Accept' : 'application/json'
+						}
+					};
+
+					return $http.get(turAPIServerService.get().concat(
+							urlFormatted), config);
+				}
+			}
+		} ]);
 turingSNApp
 		.controller(
 				'TurSNMainCtrl',
@@ -210,42 +249,3 @@ turingSNApp
 										.replace(/^([^?&]+)&/, "$1?");
 							}
 						} ]);
-turingSNApp.factory('turSNSearch', [
-		'$http',
-		'turAPIServerService',
-		function($http, turAPIServerService) {
-
-			return {
-				search : function(turSiteName, query, page, _setlocale, sort, fq, tr) {
-					var data = {
-						'q' : query,
-						'p' : page,
-						'_setlocale' : _setlocale,
-						'sort' : sort,
-						'fq[]' : fq,
-						'tr[]' : tr
-					};
-					var config = {
-						params : data,
-						headers : {
-							'Accept' : 'application/json'
-						}
-					};
-
-					return $http.get(turAPIServerService.get().concat(
-							'/sn/' + turSiteName + '/search'), config);
-				},
-				searchURL : function(url) {
-					urlFormatted = url.replace("/api/",
-							"/");
-					var config = {
-						headers : {
-							'Accept' : 'application/json'
-						}
-					};
-
-					return $http.get(turAPIServerService.get().concat(
-							urlFormatted), config);
-				}
-			}
-		} ]);
