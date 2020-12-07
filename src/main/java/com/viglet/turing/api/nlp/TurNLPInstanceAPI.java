e1e56f5dcd0d5780270e172c91ed98b4022d2b24
Alexandre Oliveira <alexandre.oliveira@viglet.com>
2020-12-07 18:07:42 -0300

    NLP Rest API for Files

--- src/main/java/com/viglet/turing/api/nlp/TurNLPInstanceAPI.java
+++ src/main/java/com/viglet/turing/api/nlp/TurNLPInstanceAPI.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2019 the original author or authors. 
+ * Copyright (C) 2016-2020 the original author or authors. 
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,15 +17,25 @@
 
 package com.viglet.turing.api.nlp;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.StringWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.Random;
+import java.util.UUID;
 
 import javax.xml.bind.JAXB;
 
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,7 +44,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
 
 import com.viglet.turing.nlp.TurNLP;
 import com.viglet.turing.nlp.output.blazon.RedactionCommand;
@@ -45,6 +59,18 @@ import com.viglet.turing.persistence.model.nlp.TurNLPInstance;
 import com.viglet.turing.persistence.repository.nlp.TurNLPEntityRepository;
 import com.viglet.turing.persistence.repository.nlp.TurNLPInstanceRepository;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.extractor.EmbeddedDocumentExtractor;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.parser.ocr.TesseractOCRConfig;
+import org.apache.tika.parser.pdf.PDFParserConfig;
+import org.apache.tika.sax.BodyContentHandler;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
@@ -52,7 +78,8 @@ import io.swagger.annotations.ApiOperation;
 @RequestMapping("/api/nlp")
 @Api(tags = "Natural Language Processing", description = "Natural Language Processing API")
 public class TurNLPInstanceAPI {
-
+	private static final Logger logger = LogManager.getLogger(TurNLPInstanceAPI.class);
+	private static final String NLP_TEMP_FILE = "nlp_temp";
 	@Autowired
 	TurNLPInstanceRepository turNLPInstanceRepository;
 	@Autowired
@@ -104,13 +131,131 @@ public class TurNLPInstanceAPI {
 
 	}
 
+	@PostMapping(value = "/{id}/validate/file/blazon", produces = MediaType.APPLICATION_XML_VALUE)
+	public RedactionScript validateFile(@RequestParam("file") MultipartFile multipartFile, @PathVariable String id) {
+
+		InputStream inputStream;
+		try {
+			inputStream = multipartFile.getInputStream();
+			StringBuffer contentFile = new StringBuffer();
+			AutoDetectParser parser = new AutoDetectParser();
+			// -1 = no limit of number of characters
+			BodyContentHandler handler = new BodyContentHandler(-1);
+			Metadata metadata = new Metadata();
+
+			TesseractOCRConfig config = new TesseractOCRConfig();
+			PDFParserConfig pdfConfig = new PDFParserConfig();
+			pdfConfig.setExtractInlineImages(true);
+
+			ParseContext parseContext = new ParseContext();
+			parseContext.set(TesseractOCRConfig.class, config);
+			parseContext.set(PDFParserConfig.class, pdfConfig);
+
+			parseContext.set(Parser.class, parser);
+
+			EmbeddedDocumentExtractor embeddedDocumentExtractor = new EmbeddedDocumentExtractor() {
+				@Override
+				public boolean shouldParseEmbedded(Metadata metadata) {
+					return true;
+				}
+
+				@Override
+				public void parseEmbedded(InputStream stream, ContentHandler handler, Metadata metadata,
+						boolean outputHtml) throws SAXException, IOException {
+					File tempFile = File.createTempFile(NLP_TEMP_FILE, null);
+
+					BodyContentHandler handlerInner = new BodyContentHandler(-1);
+					AutoDetectParser parserInner = new AutoDetectParser();
+
+					Metadata metadataInner = new Metadata();
+
+					TesseractOCRConfig tesseractConfigInner = new TesseractOCRConfig();
+					PDFParserConfig pdfConfigInner = new PDFParserConfig();
+					pdfConfigInner.setExtractInlineImages(true);
+
+					ParseContext parseContextInner = new ParseContext();
+					parseContextInner.set(TesseractOCRConfig.class, tesseractConfigInner);
+					parseContextInner.set(PDFParserConfig.class, pdfConfigInner);
+
+					parseContextInner.set(Parser.class, parserInner);
+					Files.copy(stream, tempFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+					try (FileInputStream fileInputStreamInner = new FileInputStream(tempFile)) {
+						parserInner.parse(fileInputStreamInner, handlerInner, metadataInner, parseContextInner);
+						contentFile.append(handlerInner.toString());
+						
+					} catch (IOException | SAXException | TikaException e) {
+						logger.error(e);
+					} 
+					tempFile.deleteOnExit();
+				}
+			};
+
+			parseContext.set(EmbeddedDocumentExtractor.class, embeddedDocumentExtractor);
+
+			parser.parse(inputStream, handler, metadata, parseContext);
+
+			TurNLPTextValidate textValidate = new TurNLPTextValidate();
+			contentFile.append(cleanTextContent(handler.toString()));
+			textValidate.setText(contentFile.toString());
+
+			if (this.turNLPInstanceRepository.findById(id).isPresent()) {
+				TurNLPInstance turNLPInstance = this.turNLPInstanceRepository.findById(id).get();
+				turNLP.startup(turNLPInstance, textValidate.getText());
+
+				RedactionScript redationScript = blazonEntity();
+
+				return redationScript;
+
+			}
+		} catch (IOException | SAXException | TikaException e) {
+			logger.error(e);
+		}
+
+		return null;
+	}
+
 	@SuppressWarnings("unchecked")
-	@PostMapping("/{id}/validate/{format}")
-	public TurNLPValidateResponse validate(@PathVariable String id, @PathVariable String format,
-			@RequestBody TurNLPTextValidate textValidate) throws JSONException {
+	private RedactionScript blazonEntity() {
+		List<RedactionCommand> redactionCommands = new ArrayList<>();
+		RedactionScript redationScript = new RedactionScript();
+
+		redationScript.setVersion("1");
+		for (Entry<String, Object> entityType : turNLP.validate().entrySet()) {
+			if (entityType.getValue() != null) {
+				TurNLPEntity turNLPEntity = turNLPEntityRepository.findByInternalName(entityType.getKey());
+				for (String term : ((List<String>) entityType.getValue())) {
+					RedactionCommand redactionCommand = new RedactionCommand();
+					redactionCommand.setComment(turNLPEntity.getName());
+					SearchString searchString = new SearchString();
+					searchString.setMatchWholeWord(true);
+					searchString.setString(term);
+					redactionCommand.setSearchString(searchString);
+					redactionCommands.add(redactionCommand);
+				}
+			}
+		}
+
+		redationScript.setRedactionCommands(redactionCommands);
+		return redationScript;
+	}
+
+	@PostMapping("/{id}/validate/text/{format}")
+	public String validate(@PathVariable String id, @PathVariable String format,
+			@RequestBody TurNLPTextValidate textValidate) {
+		return validateText(id, format, textValidate);
+	}
+
+	private static String cleanTextContent(String text) {
+		// Remove 2 or more spaces
+		text = text.trim().replaceAll(" +", " ");
+		return text.trim();
+	}
+
+	@SuppressWarnings("unchecked")
+	private String validateText(String id, String format, TurNLPTextValidate textValidate) {
 		final String WEB_FORMAT = "web";
 		final String BLAZON_FORMAT = "blazon";
-		
+
 		if (this.turNLPInstanceRepository.findById(id).isPresent()) {
 			TurNLPInstance turNLPInstance = this.turNLPInstanceRepository.findById(id).get();
 			turNLP.startup(turNLPInstance, textValidate.getText());
@@ -129,36 +274,15 @@ public class TurNLPInstanceAPI {
 						turNLPValidateResponse.getEntities().add(turNLPEntityValidateResponse);
 					}
 				}
-				return turNLPValidateResponse;
+				return turNLPValidateResponse.toString();
 			} else if (format.equals(BLAZON_FORMAT)) {
 
-				List<RedactionCommand> redactionCommands = new ArrayList<>();
-				RedactionScript redationScript = new RedactionScript();
-				
-				redationScript.setVersion("1");
-				for (Entry<String, Object> entityType : turNLP.validate().entrySet()) {
-					if (entityType.getValue() != null) {
-						TurNLPEntity turNLPEntity = turNLPEntityRepository.findByInternalName(entityType.getKey());
-						for (String term :((List<String>)entityType.getValue())) {
-							RedactionCommand redactionCommand = new RedactionCommand();
-							redactionCommand.setComment(turNLPEntity.getName());
-							SearchString searchString = new SearchString();
-							searchString.setMatchWholeWord(true);
-							searchString.setString(term);
-							redactionCommand.setSearchString(searchString);
-							redactionCommands.add(redactionCommand);
-						}
-					}
-				}
-				
-				redationScript.setRedactionCommands(redactionCommands);
-				
+				RedactionScript redationScript = blazonEntity();
+
 				StringWriter sw = new StringWriter();
 				JAXB.marshal(redationScript, sw);
 				String xmlString = sw.toString();
-				
-				System.out.println(xmlString);
-				return null;
+				return xmlString;
 			} else {
 
 				return null;
