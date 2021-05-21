8d25882157bd6c6d33950ea1cec1b04bb4f2ac55
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2021-05-21 09:21:23 -0300

    - Spring boot 2.5.0 e Gradle 7.0.2 and using SolrJ 8.5.2

--- src/main/java/com/viglet/turing/solr/TurSolrConfiguration.java
+++ src/main/java/com/viglet/turing/solr/TurSolrConfiguration.java
@@ -3,12 +3,11 @@ package com.viglet.turing.solr;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.http.client.config.RequestConfig;
-import org.apache.http.config.Registry;
-import org.apache.http.conn.socket.ConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.solr.client.solrj.impl.HttpClientUtil;
+import org.apache.solr.client.solrj.impl.HttpClientUtil.SchemaRegistryProvider;
 import org.apache.solr.client.solrj.impl.SolrHttpRequestRetryHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,9 +17,9 @@ public class TurSolrConfiguration {
 
 	@Bean
 	public CloseableHttpClient closeableHttpClient() {
-		Registry<ConnectionSocketFactory> schemaRegistry = HttpClientUtil.getSchemaRegisteryProvider()
-				.getSchemaRegistry();
-		PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager(schemaRegistry);
+		SchemaRegistryProvider schemaRegistry = HttpClientUtil.getSchemaRegisteryProvider();
+		
+		PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager(schemaRegistry.getSchemaRegistry());
 		cm.setMaxTotal(10000);
 		cm.setDefaultMaxPerRoute(10000);
 		cm.setValidateAfterInactivity(3000);
