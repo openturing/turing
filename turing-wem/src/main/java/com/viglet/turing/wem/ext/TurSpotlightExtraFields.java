6804a111ea320ccbb85f03f82d6ec933d52e13c6
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2021-12-30 19:37:26 -0300

    Update TurSpotlightExtraFields.java

--- turing-wem/src/main/java/com/viglet/turing/wem/ext/TurSpotlightExtraFields.java
+++ turing-wem/src/main/java/com/viglet/turing/wem/ext/TurSpotlightExtraFields.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2016-2019 Alexandre Oliveira <alexandre.oliveira@viglet.com> 
- * 
+ * Copyright (C) 2016-2019 Alexandre Oliveira <alexandre.oliveira@viglet.com>
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -16,95 +16,128 @@
  */
 package com.viglet.turing.wem.ext;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.viglet.turing.wem.beans.TurMultiValue;
 import com.viglet.turing.wem.beans.TuringTag;
 import com.viglet.turing.wem.config.IHandlerConfiguration;
 import com.vignette.as.client.common.AttributeData;
+import com.vignette.as.client.common.ref.ManagedObjectVCMRef;
 import com.vignette.as.client.exception.ApplicationException;
+import com.vignette.as.client.exception.ValidationException;
 import com.vignette.as.client.javabean.ContentInstance;
+import com.vignette.as.client.javabean.ManagedObject;
 import com.vignette.logging.context.ContextLogger;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TurSpotlightExtraFields implements ExtAttributeInterface {
-	private static final ContextLogger log = ContextLogger.getLogger(TurSpotlightExtraFields.class);
+    private static final ContextLogger log = ContextLogger.getLogger(TurSpotlightExtraFields.class);
 
-	@Override
-	public TurMultiValue consume(TuringTag tag, ContentInstance ci, AttributeData attributeData,
-			IHandlerConfiguration config) throws Exception {
-		if (log.isDebugEnabled()) {
-			log.debug("Executing TurSpotlightExtraFields");
-		}
-		List<TurSpotlightContent> turSpotlightContents = new ArrayList<>();
-		Arrays.asList(ci.getRelations(tag.getSrcAttributeRelation().get(0))).forEach(attributedObject -> {
-			try {
-				Object position = attributedObject.getAttributeValue("POSITION-TUR-SPOTLIGHT-CONTENT");
-				Object title = attributedObject.getAttributeValue("TITLE-TUR-SPOTLIGHT-CONTENT");
-				Object content = attributedObject.getAttributeValue("CONTENT-TUR-SPOTLIGHT-CONTENT");
-				Object link = attributedObject.getAttributeValue("LINK-TUR-SPOTLIGHT-CONTENT");
-				TurSpotlightContent turSpotlightContent = new TurSpotlightContent((int) position, (String) title,
-						(String) content, (String) link);
-				turSpotlightContents.add(turSpotlightContent);
-			} catch (ApplicationException e) {
-				log.error(e.getMessage(), e);
-			}
-		});
-		ObjectMapper mapper = new ObjectMapper();
-		String json = mapper.writeValueAsString(turSpotlightContents);
-		TurMultiValue turMultiValue = new TurMultiValue();
-		turMultiValue.add(json);
+    @Override
+    public TurMultiValue consume(TuringTag tag, ContentInstance ci, AttributeData attributeData,
+                                 IHandlerConfiguration config) throws Exception {
+        if (log.isDebugEnabled()) {
+            log.debug("Executing TurSpotlightExtraFields");
+        }
+        List<TurSpotlightContent> turSpotlightContents = new ArrayList<>();
+        Arrays.asList(ci.getRelations(tag.getSrcAttributeRelation().get(0))).forEach(attributedObject -> {
+            try {
+                Object position = attributedObject.getAttributeValue("POSITION-TUR-SPOTLIGHT-CONTENT");
+                Object title = attributedObject.getAttributeValue("TITLE-TUR-SPOTLIGHT-CONTENT");
+                Object content = attributedObject.getAttributeValue("CONTENT-TUR-SPOTLIGHT-CONTENT");
+                Object link = attributedObject.getAttributeValue("LINK-TUR-SPOTLIGHT-CONTENT");
 
-		return turMultiValue;
-	}
+                ContentInstance contentInstance = (ContentInstance) ManagedObject
+                        .findByContentManagementId(new ManagedObjectVCMRef(content.toString()));
+                TurCTDAttributes turCTDAttributes = new TurCTDAttributes();
+                turCTDAttributes.setId((String) content);
+                turCTDAttributes.setTitle((String) contentInstance.getAttributeValue("title"));
+                String ctdJson = new ObjectMapper().writeValueAsString(turCTDAttributes);
 
-	class TurSpotlightContent {
-		private int position;
-		private String title;
-		private String content;
-		private String link;
+                TurSpotlightContent turSpotlightContent = new TurSpotlightContent((int) position, (String) title,
+                        ctdJson, (String) link);
+                turSpotlightContents.add(turSpotlightContent);
+            } catch (ApplicationException | ValidationException | JsonProcessingException e) {
+                log.error(e.getMessage(), e);
+            }
+        });
 
-		public TurSpotlightContent(int position, String title, String content, String link) {
-			super();
-			this.position = position;
-			this.title = title;
-			this.content = content;
-			this.link = link;
-		}
+        String json = new ObjectMapper().writeValueAsString(turSpotlightContents);
+        TurMultiValue turMultiValue = new TurMultiValue();
+        turMultiValue.add(json);
 
-		public int getPosition() {
-			return position;
-		}
+        return turMultiValue;
+    }
 
-		public void setPosition(int position) {
-			this.position = position;
-		}
+    class TurCTDAttributes {
+        private String id;
+        private String title;
 
-		public String getTitle() {
-			return title;
-		}
+        public String getId() {
+            return id;
+        }
 
-		public void setTitle(String title) {
-			this.title = title;
-		}
+        public void setId(String id) {
+            this.id = id;
+        }
 
-		public String getContent() {
-			return content;
-		}
+        public String getTitle() {
+            return title;
+        }
 
-		public void setContent(String content) {
-			this.content = content;
-		}
+        public void setTitle(String title) {
+            this.title = title;
+        }
+    }
 
-		public String getLink() {
-			return link;
-		}
+    class TurSpotlightContent {
+        private int position;
+        private String title;
+        private String content;
+        private String link;
 
-		public void setLink(String link) {
-			this.link = link;
-		}
+        public TurSpotlightContent(int position, String title, String content, String link) {
+            super();
+            this.position = position;
+            this.title = title;
+            this.content = content;
+            this.link = link;
+        }
 
-	}
+        public int getPosition() {
+            return position;
+        }
+
+        public void setPosition(int position) {
+            this.position = position;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        public String getLink() {
+            return link;
+        }
+
+        public void setLink(String link) {
+            this.link = link;
+        }
+
+    }
 }
