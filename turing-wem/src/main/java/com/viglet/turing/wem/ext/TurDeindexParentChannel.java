c1084556cc5d775100c9637d3a9509465b8cfbdf
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2022-10-06 16:14:13 -0300

    - WEM: Comments Apache 2.0 License

--- turing-wem/src/main/java/com/viglet/turing/wem/ext/TurDeindexParentChannel.java
+++ turing-wem/src/main/java/com/viglet/turing/wem/ext/TurDeindexParentChannel.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2016-2022 the original author or authors. 
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.viglet.turing.wem.ext;
 
 import com.viglet.turing.wem.broker.indexer.TurWEMDeindex;
@@ -7,7 +27,6 @@ import com.viglet.turing.wem.util.TuringUtils;
 import com.vignette.as.client.common.WhereClause;
 import com.vignette.as.client.common.ref.ChannelRef;
 import com.vignette.as.client.common.ref.ManagedObjectVCMRef;
-import com.vignette.as.client.javabean.Channel;
 import com.vignette.as.client.javabean.ContentInstance;
 
 public class TurDeindexParentChannel implements IValidToIndex {
@@ -21,16 +40,12 @@ public class TurDeindexParentChannel implements IValidToIndex {
 		if (channelRefs.length > 0) {
 			String siteName = TuringUtils.getSiteNameFromContentInstance(ci, config);
 			for (ChannelRef channelRef : channelRefs) {
-				ManagedObjectVCMRef parentChannel = getParentChannelFromBreadcrumb(
+				ManagedObjectVCMRef parentChannel = TuringUtils.getParentChannelFromBreadcrumb(
 						channelRef.getChannel().getBreadcrumbPath(true));
 				TurWEMDeindex.indexDelete(parentChannel, config, siteName);
 			}
 		}
 		return true;
-	}
-
-	private ManagedObjectVCMRef getParentChannelFromBreadcrumb(Channel[] breadcrumb) {
-		return breadcrumb[breadcrumb.length - 1].getContentManagementId();
 	}
 
 	@Override
