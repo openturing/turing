8747ad74225487a8476f031f3c3c328a2c6dc175
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2023-08-21 18:49:50 -0300

    - Spring Boot setUseTrailingSlashMatch

--- turing-app/src/main/java/com/viglet/turing/spring/TurStaticResourceConfiguration.java
+++ turing-app/src/main/java/com/viglet/turing/spring/TurStaticResourceConfiguration.java
@@ -32,6 +32,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -42,12 +43,18 @@ import org.springframework.web.servlet.resource.PathResourceResolver;
 public class TurStaticResourceConfiguration implements WebMvcConfigurer {
 	@Value("${turing.allowedOrigins:localhost}")
 	private String allowedOrigins;
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/api/**").allowedOrigins(allowedOrigins).allowedMethods("PUT", "DELETE", "GET", "POST")
 				.allowCredentials(false).maxAge(3600);
 	}
 
+	@Override
+	public void configurePathMatch(PathMatchConfigurer configurer) {
+		configurer.setUseTrailingSlashMatch(true);
+	}
+	 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/console").setViewName("forward:/console/index.html");
@@ -63,21 +70,19 @@ public class TurStaticResourceConfiguration implements WebMvcConfigurer {
 	public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
 		configurer.setDefaultTimeout(-1);
 	}
-	
-	@Override
-   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/console/**")
-                .addResourceLocations("classpath:/public/console/")
-                .resourceChain(true)
-                .addResolver(new PathResourceResolver() {
-                    @Override
-                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
-                        Resource requestedResource = location.createRelative(resourcePath);
- 
-                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
-                                : new ClassPathResource("/public/console/index.html");
-                    }
-                });
 
-    }
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/console/**").addResourceLocations("classpath:/public/console/")
+				.resourceChain(true).addResolver(new PathResourceResolver() {
+					@Override
+					protected Resource getResource(String resourcePath, Resource location) throws IOException {
+						Resource requestedResource = location.createRelative(resourcePath);
+
+						return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+								: new ClassPathResource("/public/console/index.html");
+					}
+				});
+
+	}
 }
