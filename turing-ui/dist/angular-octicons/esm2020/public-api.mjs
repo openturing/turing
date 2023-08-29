47f156a0be4336eef01077da2c6d351e8ad23724
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2023-08-28 22:23:48 -0300

    removed dist directory from turing-ui

--- turing-ui/dist/angular-octicons/esm2020/public-api.mjs
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of angular-octicons
- */
-export * from './lib/angular-octicons.module';
-export * from './lib/octicon.directive';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHVibGljLWFwaS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uL3Byb2plY3RzL2FuZ3VsYXItb2N0aWNvbnMvc3JjL3B1YmxpYy1hcGkudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7O0dBRUc7QUFFSCxjQUFjLCtCQUErQixDQUFDO0FBQzlDLGNBQWMseUJBQXlCLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyIvKlxuICogUHVibGljIEFQSSBTdXJmYWNlIG9mIGFuZ3VsYXItb2N0aWNvbnNcbiAqL1xuXG5leHBvcnQgKiBmcm9tICcuL2xpYi9hbmd1bGFyLW9jdGljb25zLm1vZHVsZSc7XG5leHBvcnQgKiBmcm9tICcuL2xpYi9vY3RpY29uLmRpcmVjdGl2ZSc7XG4iXX0=
