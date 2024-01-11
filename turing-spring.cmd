c05783102629f577366d7520e036adae627d8e98
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2024-01-11 10:26:26 -0300

    - UI fixed site redirect when delete it

--- turing-spring.cmd
+++ turing-spring.cmd
@@ -1,1 +1,3 @@
-mvn -Dmaven.repo.local=D:\repo spring-boot:run -pl turing-app -Dskip.npm
+call mvn clean install -pl turing-commons
+call mvn clean install -pl turing-java-sdk
+call mvn -Dmaven.repo.local=D:\repo clean spring-boot:run -pl turing-app -Dskip.npm
