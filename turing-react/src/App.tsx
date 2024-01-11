a6bee1d51ad9193c90cf21f421c3bce941d6d8b2
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2024-01-11 11:00:11 -0300

    - React Primer

--- turing-react/src/App.tsx
+++ turing-react/src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Box, Heading, ThemeProvider} from '@primer/react'
 
+function TurBase() {
+    return (
+        <Box m={4}>
+            <Heading as="h2" sx={{mb: 2}}>
+                Hello, world!
+            </Heading>
+            <p>This will get Primer text styles.</p>
+        </Box>
+    )
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ThemeProvider>
+            <TurBase/>
+        </ThemeProvider>
+    )
 }
 
 export default App;
