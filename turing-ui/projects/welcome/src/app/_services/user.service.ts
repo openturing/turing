c577dc1398730e22363fd943f15ebc9d3463268d
Alexandre Oliveira <alexandre.oliveira@gmail.com>
2024-07-12 18:29:35 -0300

    - UI: Merge evironment.ts files

--- turing-ui/projects/welcome/src/app/_services/user.service.ts
+++ turing-ui/projects/welcome/src/app/_services/user.service.ts
@@ -1,9 +1,9 @@
 ﻿import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { environment } from '../../environments/environment';
 import { User } from '../_models';
 import { Observable } from 'rxjs';
+import {environment} from "../../../../../environments/environment";
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
