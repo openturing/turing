2e65aa43224b700b161ce11ef8f43a3203505e0b
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2024-10-04 19:44:15 -0300

    - AC: State Machine Refactory.

--- turing-app/src/main/java/com/viglet/turing/sn/ac/TurSNSuggestionAutomaton.java
+++ turing-app/src/main/java/com/viglet/turing/sn/ac/TurSNSuggestionAutomaton.java
@@ -3,6 +3,9 @@ package com.viglet.turing.sn.ac;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineBuilder;
 
 import java.util.*;
 
@@ -17,7 +20,7 @@ import java.util.*;
  * automaton.</li>
  * <li>{@link #isAddSuggestion(String, int, List)}: Runs the automaton on a given
  * suggestion, number of words from query, and stop words list.</li>
- * <li>{@link #getTokenType(List, String, boolean)}: Determines the token type
+ * <li>{@link #getTokenType(List, String)}: Determines the token type
  * of a given token.</li>
  * </ul>
  *
@@ -26,12 +29,10 @@ import java.util.*;
  */
 @Slf4j
 public class TurSNSuggestionAutomaton {
-    public static final String N0 = "N0";
-    public static final String N1 = "N1";
-    public static final String N2 = "N2";
-    public static final String N3 = "N3";
     public static final String ACCEPT = "Accept";
     public static final String ERROR = "Error";
+    public static final String FIRST_TERM_OR_STOP_WORD = "FirstTermOrStopWord";
+    public static final String PREVIOUS_TERM_IS_WORD = "PreviousTermIsWord";
     private State initialState;
 
     public TurSNSuggestionAutomaton() {
@@ -42,26 +43,21 @@ public class TurSNSuggestionAutomaton {
      * This constructs the finite state machine structure and binds to <code>initialState</code> parameter.
      */
     private void buildAutomaton() {
-        State n0 = new State(N0);
-        State n1 = new State(N1);
-        State n2 = new State(N2);
-        State n3 = new State(N3);
+        State firstTermOrStopWord = new State(FIRST_TERM_OR_STOP_WORD);
+        State previousTermIsWord = new State(PREVIOUS_TERM_IS_WORD);
         State accept = new State(ACCEPT, State.StateType.ACCEPT);
         State reject = new State(ERROR, State.StateType.REJECT);
 
-        setTransitions(n0, n1, reject, reject, n2);
-        setTransitions(n1, reject, reject, accept, n2);
-        setTransitions(n2, n3, n2, reject, n2);
-        setTransitions(n3, reject, reject, accept, reject);
+        setTransitions(firstTermOrStopWord, previousTermIsWord, firstTermOrStopWord, reject);
+        setTransitions(previousTermIsWord, reject, reject, accept);
 
-        this.initialState = n0;
+        this.initialState = firstTermOrStopWord;
     }
 
-    private static void setTransitions(State state, State word, State stopWord, State empty, State specialStopWord) {
+    private static void setTransitions(State state, State word, State stopWord, State empty) {
         state.transitions.put(TokenType.WORD, word);
         state.transitions.put(TokenType.STOP_WORD, stopWord);
         state.transitions.put(TokenType.EMPTY, empty);
-        state.transitions.put(TokenType.SPECIAL_STOP_WORD, specialStopWord);
     }
 
     /**
@@ -73,27 +69,8 @@ public class TurSNSuggestionAutomaton {
      * @return {@code true} if the suggestion is valid, according to the automaton rules, {@code false} otherwise.
      */
     public boolean isAddSuggestion(String suggestion, int numberOfWordsFromQuery, List<String> stopWords) {
-        // TOP -> [ "Hello", "World" ]
-        Deque<String> tokensDeque = new ArrayDeque<>(List.of(suggestion.split(" ")));
-
-        // Suggestions should not start with a stop word when is the first term of the query.
-        if (stopWords.contains(tokensDeque.peek()) && numberOfWordsFromQuery == 1) {
-            return false;
-        }
-
-        // The suggestions will always include the query, so we need to ignore it.
-        int wordsToRemove = numberOfWordsFromQuery - 1;
-        // Query: "Hello my friend" → numberOfWordsFromQuery = 3
-        // Query: "Hello my friend " → numberOfWordsFromQuery = 4
-        while (wordsToRemove > 0 && !tokensDeque.isEmpty()) {
-            tokensDeque.pop();
-            wordsToRemove--;
-        }
-
-        // Checks if now it starts with a stop word
-        boolean firstTokenIsStopWord = stopWords.contains(tokensDeque.peek());
-
-        if (tokensDeque.isEmpty()) {
+        final Deque<String> tokensDeque = prepareNewSuggestionTokens(suggestion, numberOfWordsFromQuery, stopWords);
+        if (tokensDeque == null || tokensDeque.isEmpty()) {
             log.debug("Suggestion is empty.");
             return false;
         }
@@ -110,7 +87,7 @@ public class TurSNSuggestionAutomaton {
             }
 
             currentToken = tokensDeque.poll();
-            currentTokenType = getTokenType(stopWords, currentToken, firstTokenIsStopWord);
+            currentTokenType = getTokenType(stopWords, currentToken);
 
             log.debug("Current token: {} - Type: {}", currentToken, currentTokenType);
             log.debug("Current state: {}", currentState.name);
@@ -119,16 +96,36 @@ public class TurSNSuggestionAutomaton {
         }
     }
 
+    @Nullable
+    private static Deque<String> prepareNewSuggestionTokens(String suggestion, int numberOfWordsFromQuery,
+                                                            List<String> stopWords) {
+        // TOP -> [ "Hello", "World" ]
+        Deque<String> tokensDeque = new ArrayDeque<>(List.of(suggestion.split(" ")));
+
+        // Suggestions should not start with a stop word when is the first term of the query.
+        if (stopWords.contains(tokensDeque.peek()) && numberOfWordsFromQuery == 1) {
+            return null;
+        }
+
+        // The suggestions will always include the query, so we need to ignore it.
+        int wordsToRemove = numberOfWordsFromQuery - 1;
+        // Query: "Hello my friend" → numberOfWordsFromQuery = 3
+        // Query: "Hello my friend " → numberOfWordsFromQuery = 4
+        while (wordsToRemove > 0 && !tokensDeque.isEmpty()) {
+            tokensDeque.pop();
+            wordsToRemove--;
+        }
+        return tokensDeque;
+    }
+
     @NotNull
-    private TokenType getTokenType(List<String> stopWords, String currentToken, boolean firstTokenIsStopWord) {
+    private TokenType getTokenType(List<String> stopWords, String currentToken) {
         TokenType tokenType;
         if (StringUtils.isEmpty(currentToken)) {
             return TokenType.EMPTY;
         }
         if (stopWords.contains(currentToken)) {
             tokenType = TokenType.STOP_WORD;
-            if (firstTokenIsStopWord)
-                tokenType = TokenType.SPECIAL_STOP_WORD;
         } else {
             tokenType = TokenType.WORD;
         }
@@ -138,7 +135,6 @@ public class TurSNSuggestionAutomaton {
     private enum TokenType {
         WORD,
         STOP_WORD,
-        SPECIAL_STOP_WORD,
         EMPTY
     }
 
