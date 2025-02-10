e4f2c074ece91853114d3d6b5da8b705ea42eb98
Alê Oliveira <alexandre.oliveira@gmail.com>
2025-02-10 18:20:51 -0300

    - Removed React module

--- turing-react/src/axios/axiosSetup.ts
+++
@@ -1,7 +0,0 @@
-import axios, {AxiosInstance, AxiosRequestConfig} from 'axios';
-
-const initialization = (config: AxiosRequestConfig): AxiosInstance => {
-    return axios.create(config);
-};
-
-export default initialization;
--- turing-react/src/models/TurListProps.ts
+++
@@ -1,6 +0,0 @@
-import {Observable} from "rxjs";
-
-export interface TurListProps<T>  {
-    getAll: (url: string) => Observable<Array<T>>;
-    change: Observable<boolean> | undefined;
-}
